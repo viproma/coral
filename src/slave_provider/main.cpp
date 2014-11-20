f9473ef33210181cb526df4916a9bbf532affdc0
Lars T. Kyllingstad <lars.kyllingstad@sintef.no>
2014-11-20 14:25:35 +0100

    Slave exe no longer hardcoded in slave provider

--- src/slave_provider/main.cpp
+++ src/slave_provider/main.cpp
@@ -1,41 +1,50 @@
+#include <cstdlib>
 #include <exception>
 #include <iostream>
 #include <string>
 #include <vector>
 
 #include "boost/filesystem.hpp"
+#include "boost/foreach.hpp"
 #include "boost/lexical_cast.hpp"
+#include "boost/program_options.hpp"
 
 #include "dsb/domain/slave_provider.hpp"
 #include "dsb/fmi.hpp"
 #include "dsb/util.hpp"
 
 
-void StartSlave(
-    dsb::model::SlaveID slaveID,
-    const dsb::execution::Locator& executionLocator,
-    const std::string& fmuPath)
+struct StartSlave
 {
-    const auto slaveIdString = boost::lexical_cast<std::string>(slaveID);
-    const auto outputFile = "slave_" + slaveIdString + ".csv";
-    std::vector<std::string> args;
-    args.push_back(slaveIdString);
-    args.push_back(executionLocator.SlaveEndpoint());
-    args.push_back(executionLocator.VariablePubEndpoint());
-    args.push_back(executionLocator.VariableSubEndpoint());
-    args.push_back(fmuPath);
-    args.push_back(outputFile);
+public:
+    StartSlave(const std::string& slaveExe) : m_slaveExe(slaveExe) { }
 
-    std::cout << "\nStarting slave " << slaveID << '\n'
-        << "  Execution: " << executionLocator.SlaveEndpoint() << '\n'
-        << "  FMU      : " << fmuPath << '\n'
-        << "  Output   : " << outputFile << '\n'
-        << std::flush;
-    // TODO: You know what:
-    dsb::util::SpawnProcess(
-        "C:\\Users\\larky\\Development\\viproma\\dsb\\build32\\src\\slave\\Debug\\slave.exe",
-        args);
-}
+    void operator()(
+        dsb::model::SlaveID slaveID,
+        const dsb::execution::Locator& executionLocator,
+        const std::string& fmuPath)
+    {
+        const auto slaveIdString = boost::lexical_cast<std::string>(slaveID);
+        const auto outputFile = "slave_" + slaveIdString + ".csv";
+        std::vector<std::string> args;
+        args.push_back(slaveIdString);
+        args.push_back(executionLocator.SlaveEndpoint());
+        args.push_back(executionLocator.VariablePubEndpoint());
+        args.push_back(executionLocator.VariableSubEndpoint());
+        args.push_back(fmuPath);
+        args.push_back(outputFile);
+
+        std::cout << "\nStarting slave " << slaveID << '\n'
+            << "  Execution: " << executionLocator.SlaveEndpoint() << '\n'
+            << "  FMU      : " << fmuPath << '\n'
+            << "  Output   : " << outputFile << '\n'
+            << std::flush;
+        dsb::util::SpawnProcess(m_slaveExe, args);
+    }
+
+private:
+    std::string m_slaveExe;
+};
 
 
 void ScanDirectoryForFMUs(const std::string& directory, std::vector<std::string>& fmuPaths)
@@ -55,31 +64,76 @@ void ScanDirectoryForFMUs(const std::string& directory, std::vector<std::string>
 int main(int argc, const char** argv)
 {
 try {
-    if (argc < 4) {
+    namespace po = boost::program_options;
+    po::options_description optDesc("Options");
+    optDesc.add_options()
+        ("help",      "Display help message")
+        ("slave-exe", po::value<std::string>(), "The path to the DSB slave executable");
+    po::options_description argDesc;
+    argDesc.add(optDesc);
+    argDesc.add_options()
+        ("domain",    po::value<std::string>(),                    "The address of the domain")
+        ("fmu",       po::value<std::vector<std::string>>(),       "The FMU files and directories");
+    po::positional_options_description posArgDesc;
+    posArgDesc.add("domain", 1)
+              .add("fmu", -1);
+    po::variables_map optMap;
+    po::store(po::command_line_parser(argc, argv).options(argDesc)
+                                                 .positional(posArgDesc)
+                                                 .run(),
+              optMap);
+
+    if (argc < 2 || optMap.count("help")) {
         const auto self = boost::filesystem::path(argv[0]).stem().string();
-        std::cerr << "Usage: " << self << " <report> <info> <fmus...>\n"
-                  << "  report   = Slave provider report endpoint (e.g. tcp://myhost:5432)\n"
-                  << "  info     = Slave provider info endpoint (e.g. tcp://myhost:5432)\n"
-                  << "  fmus     = Paths to FMI1 FMU and/or directories containing FMUs.\n"
-                  << "             Directories will be scanned recursively for files with .fmu extensions."
-                  << std::endl;
+        std::cerr << "Usage: " << self << " <domain> <fmus...> [options...]\n"
+                  << "Arguments:\n"
+                  << "  domain   The domain address\n"
+                  << "  fmus     FMU files and directories\n"
+                  << optDesc << std::endl;
         return 0;
     }
-    const auto reportEndpoint = std::string(argv[1]);
-    const auto infoEndpoint = std::string(argv[2]);
-    std::vector<std::string> fmuPaths;
-    for (auto i = 3; i < argc; ++i) {
-        if (boost::filesystem::is_directory(argv[i])) {
-            ScanDirectoryForFMUs(argv[i], fmuPaths);
+
+    if (!optMap.count("domain")) throw std::runtime_error("Domain address not specified");
+    if (!optMap.count("fmu")) throw std::runtime_error("No FMUs specified");
+
+    const auto domainAddress = optMap["domain"].as<std::string>();
+    const auto reportEndpoint = domainAddress + ":51381";
+    const auto infoEndpoint = domainAddress + ":51383";
+
+    std::string slaveExe;
+    if (optMap.count("slave-exe")) {
+        slaveExe = optMap["slave-exe"].as<std::string>();
+    } else if (const auto slaveExeEnv = std::getenv("DSB_SLAVE_EXE")) {
+        slaveExe = slaveExeEnv;
+    } else {
+#ifdef _WIN32
+        const auto exeName = "slave.exe";
+#else
+        const auto exeName = "slave";
+#endif
+        auto tryPath = dsb::util::ThisExePath().parent_path() / exeName;
+        if (boost::filesystem::exists(tryPath)) {
+            slaveExe = tryPath.string();
         } else {
-            fmuPaths.push_back(argv[i]);
+            throw std::runtime_error("Slave executable not specified or found");
+        }
+    }
+
+    assert (!slaveExe.empty());
+
+    std::vector<std::string> fmuPaths;
+    BOOST_FOREACH (const auto& fmuSpec, optMap["fmu"].as<std::vector<std::string>>()) {
+        if (boost::filesystem::is_directory(fmuSpec)) {
+            ScanDirectoryForFMUs(fmuSpec, fmuPaths);
+        } else {
+            fmuPaths.push_back(fmuSpec);
         }
     }
 
     std::vector<std::unique_ptr<dsb::domain::ISlaveType>> fmus;
     std::vector<dsb::domain::ISlaveType*> fmuPtrs;
     for (auto it = fmuPaths.begin(); it != fmuPaths.end(); ++it) {
-        fmus.push_back(dsb::fmi::MakeSlaveType(*it, StartSlave));
+        fmus.push_back(dsb::fmi::MakeSlaveType(*it, StartSlave(slaveExe)));
         fmuPtrs.push_back(fmus.back().get());
         std::clog << "FMU loaded: " << *it << std::endl;
     }
