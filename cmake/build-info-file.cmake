36ab8ab276f3407c78d1cc6bff13a44782297c16
Lars T. Kyllingstad <lars.kyllingstad@sintef.no>
2017-01-24 17:19:46 +0100

    Trivial: Fix for CMake generator differences

--- cmake/build-info-file.cmake
+++ cmake/build-info-file.cmake
@@ -7,7 +7,7 @@
 set (_buildInfoFile "${CMAKE_BINARY_DIR}/build-info.txt")
 
 find_package(Git)
-if(Git_FOUND)
+if(Git_FOUND OR GIT_FOUND)
     set(_gitCommand "${GIT_EXECUTABLE}" "log" "-n1" "--pretty=format:Git commit: %H %ci%n")
 else()
     set(_gitCommand "${CMAKE_COMMAND}" "-E" "echo" "Git commit: (undetermined)")
@@ -16,7 +16,8 @@ endif()
 add_custom_command(
     OUTPUT "${_buildInfoFile}"
     COMMAND ${_gitCommand} ">" "${_buildInfoFile}"
-    COMMAND "${CMAKE_COMMAND}" "-E" "echo" "Build type: $<CONFIG>" ">>" "${_buildInfoFile}"
+    COMMAND "${CMAKE_COMMAND}" "-E" "echo" "Build type: $<CONFIG>${CMAKE_BUILD_TYPE}" ">>" "${_buildInfoFile}"
+    WORKING_DIRECTORY "${CMAKE_SOURCE_DIR}"
     COMMENT "Generating build information file"
     VERBATIM
 )
